07ac5b103732445f42630c275d07fae2be8ebe3b
fang1712 <fanghong1712@gmail.com>
2021-12-19 03:14:43 +0000

    firststep

---
+++ src/routes/index.svelte
@@ -0,0 +1,8 @@
+
+<h1>Weather App</h1>
+
+<form on:submit|preventDefault={getWeather}>
+    <label for="location">Location:</label>
+    <input id="location" bind:value={location} type="text" />
+    <button type="submit">Check</button>
+</form>
